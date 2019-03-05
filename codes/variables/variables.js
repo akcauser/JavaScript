6fc35fb95b7514a956245c7c76872589f9904d8b
Ertuğrul Gazi AKÇA <akcaertugrulgazi@gmail.com>
2019-03-05 22:28:58 +0300

    Add files via upload
    variables added

---
+++ codes/variables/variables.js
@@ -0,0 +1,18 @@
+var stringVariable = "Merhaba";
+var integerVariable = 5;
+var doubleVariable = 4.3;
+
+var db = "mongodb"
+
+
+console.log("String: ",stringVariable);
+console.log("İnteger: ",integerVariable);
+console.log("Double: ",doubleVariable);
+
+var a = 5,
+    b = 6,
+    c = 7;
+
+console.log(a,b,c);    
+
+// Değişken tanımlarken rakam ile başlanmaz. 
