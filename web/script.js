4dbb3852ab19da45023e299bd63689b5f9aab506
akcauser <akcaertugrulgazi@gmail.com>
2019-11-13 15:23:57 +0300

    web repository added

---
+++ web/script.js
@@ -0,0 +1,12 @@
+function func(){
+    document.getElementById("pID").innerHTML = "imported function";
+}
+function funcDocumentWrite(text){
+    document.write(text);
+}
+function funcConsoleLog(){
+    console.log("logged something");
+}
+function windowAlert(){
+    window.alert("This is an alert");
+}
